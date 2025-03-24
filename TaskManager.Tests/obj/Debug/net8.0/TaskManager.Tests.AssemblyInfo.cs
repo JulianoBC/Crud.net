42c408aa77aa83bbd710d08bfbbe5355b1a429f8
JulianoBC <jbcap@hotmail.com>
2025-03-24 00:28:11 -0300

    ajustes

--- TaskManager.Tests/obj/Debug/net8.0/TaskManager.Tests.AssemblyInfo.cs
+++ TaskManager.Tests/obj/Debug/net8.0/TaskManager.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskManager.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ca4bfbb654a9a4a00ff8607e1b45ac01b5046d4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7636108dad62d4429498b5cbba44e03453e85000")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskManager.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskManager.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
